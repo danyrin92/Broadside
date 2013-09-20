8150ec0bbb4e1efb34c2735e1064dbb11dfe889a
CaptWalloper <comccoy@cfl.rr.com>
2013-09-19 21:26:35 -0400

    boom

--- BroadsideRepo/Broadside/src/com/starboardstudios/broadside/structures/turrets/Turret.java
+++ BroadsideRepo/Broadside/src/com/starboardstudios/broadside/structures/turrets/Turret.java
@@ -2,4 +2,28 @@ package com.starboardstudios.broadside.structures.turrets;
 
 public abstract class Turret {
 
+	//properties
+	  //image
+	  //position
+	//Projectile projectile;
+	boolean canAffect;
+	double range;
+	
+	//constructors
+	  //none: abstract class
+	
+	//methods
+	void spawn() {
+		
+	}
+	void destroy() {
+		
+	}
+	void fire() {
+		
+	}
+	void randomFire() {
+		
+	}
+	
 }
