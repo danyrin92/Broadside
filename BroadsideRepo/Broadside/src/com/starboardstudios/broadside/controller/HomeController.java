d43d29dbfdbaa483d976aa2a6555b0eee076fdb7
CaptWalloper <comccoy@cfl.rr.com>
2013-10-09 13:44:20 -0400

    Testing Update

--- BroadsideRepo/Broadside/src/com/starboardstudios/broadside/controller/HomeController.java
+++ BroadsideRepo/Broadside/src/com/starboardstudios/broadside/controller/HomeController.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import com.starboardstudios.broadside.R;
-
+//COMMENT YO SHIT
 public class HomeController extends BaseController{
 	
 	@Override
