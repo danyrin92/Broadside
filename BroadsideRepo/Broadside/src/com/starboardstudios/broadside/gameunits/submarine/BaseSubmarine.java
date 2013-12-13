f020625f64ceb982cd607da16fe8546ff154bdbc
glogan <glogan92@ufl.edu>
2013-12-12 23:49:17 -0500

    Updating Ship Collision Implementation

--- BroadsideRepo/Broadside/src/com/starboardstudios/broadside/gameunits/submarine/BaseSubmarine.java
+++ BroadsideRepo/Broadside/src/com/starboardstudios/broadside/gameunits/submarine/BaseSubmarine.java
@@ -1,4 +1,4 @@
-package com.starboardstudios.broadside.gameunits.submarine;
+package com.starboardstudios.broadside.gameunits.aircrafts;
 
 import android.view.View;
 import android.widget.ImageView;
@@ -10,21 +10,20 @@ import com.starboardstudios.broadside.gameunits.ships.MainShip;
 
 import java.util.Random;
 
-  public abstract class BaseSubmarine extends com.starboardstudios.broadside.gameunits.CombatUnit {
+public abstract class BaseAircraft extends CombatUnit {
 
 	/** Random value for choosing pathing track */
 	Random rand = new Random();
-	int random = rand.nextInt(1);
+	int random = rand.nextInt(3);
 
-	public BaseSubmarine(Model model) {
+	public BaseAircraft(Model model) {
 		super(model.context);
 		this.model = model;
 
 		imageView.setVisibility(View.INVISIBLE);
 		imageView.setAdjustViewBounds(true);
-	}
 
-	
+	}
 
 	@Override
 	public ImageView getImage() {
@@ -34,29 +33,6 @@ import java.util.Random;
 	public void setVelocity(int xSpeed, int ySpeed) {
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
-	}
-
-	public void collide(BaseUnit unit) {
-		if (unit instanceof Projectile) {
-			if (((Projectile)unit).creator instanceof MainShip) {
-				damage(((Projectile) unit).getDamage());
-				unit.collide(this);
-			}
-		}
-	}
-	
-	public void damage(int damage) {
-		health -= damage;
-		//TODO: Add animation to the damage method
-		if (health <= 0) {
-			destroy();
-		}
-	}
-	
-	public void destroy() {
-		//TODO: Add animations to the destroy method
-		model.removeUnit(this);
-		model.addBooty(plunder);
 	}
 
 }
